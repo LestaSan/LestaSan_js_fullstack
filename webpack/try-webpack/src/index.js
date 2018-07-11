677c4e5c5cfe912a02b53428dfe2ae8b3bce6c30
LestaSan <295510545@qq.com>
2018-07-11 21:09:14 +0800

    webpack学习

--- webpack/try-webpack/src/index.js
+++ webpack/try-webpack/src/index.js
@@ -1,5 +1,14 @@
 // 文件引用 按文件引用顺序打包文件 依赖
-require('./style.less')
+// require('./style.less/')   
+require('./style/normalize')   
+require('./style/index')   
+const format = require('utils/format')
 const { log } = require('./utils')
 
-log('hello world')
+log(format('hello world'))
+document.querySelector('.superman').style.display = 'block'
+
+log(_.map([1,2,3], (item) => item * 2))
+
+// log('hello world')
+
--- webpack/try-webpack/webpack.config.js
+++ webpack/try-webpack/webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
-// console.log(path.resolve(__dirname, 'dist'));
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+console.log(path.resolve(__dirname, 'dist'))
 module.exports = {
     entry: './src/index.js',
     output: {
@@ -8,13 +11,20 @@ module.exports = {
         filename: '[name].js'
     },
     module: {
-        rules:[
+        rules: [
             {
                 test: /\.js$/,
                 include: [
                     path.resolve(__dirname, 'src')
                 ],
                 use: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader']
+                })
             },
             {
                 test: /\.less$/,
@@ -25,10 +35,50 @@ module.exports = {
                         'less-loader'
                     ]
                 })
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader'
+                    }
+                ]
             }
         ]
     },
+    resolve: {
+        alias: {
+            utils: path.resolve(__dirname, 'src/utils')
+        },
+        extensions: ['.js', '.json', '.css', '.less']
+    },
     plugins: [
-        new ExtractTextPlugin('[name].css')
-    ] 
+        new ExtractTextPlugin('[name].css'),
+        new HtmlWebpackPlugin({
+            file: 'index.html',
+            template: 'src/index.html'
+        }),
+        new CopyWebpackPlugin([
+            {
+                from: 'src/assets/favicon.ico',
+                to: 'favicon.ico'
+            }
+        ]),
+        // loadsh作为工具库，是很多组件会使用的
+        // 省去了到处import
+        new webpack.ProvidePlugin({
+            '_': 'lodash'
+        })
+    ],
+    devServer: {
+        port: '1314',
+        before(app) {
+            app.get('/api/test.json', (req, res) => {
+                res.json({
+                    code: 200,
+                    message: 'hello world'
+                })
+            })
+        }
+    }
 }

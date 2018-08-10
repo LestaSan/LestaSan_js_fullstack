dfe033ee44eb2bad636cf8797ad6040933cde62f
Lesta <295510545@qq.com>
2018-08-10 22:00:14 +0800

    koa学习

---
+++ node/koa-blog/config/default.js
@@ -0,0 +1,12 @@
+const config = {
+    port: 3000,
+    database: {
+        DATABASE: 'nodesql',
+        USERNAME: 'root',
+        PASSWORD: '12345678',
+        POST: '3306',
+        HOST: 'localhost'
+    }
+}
+
+module.exports = config;
---
+++ node/koa-blog/controller/c-posts.js
@@ -0,0 +1,9 @@
+
+
+exports.getRedirectPosts = async ctx => {
+    ctx.redirect('/posts');
+}
+
+exports.getPosts = async ctx => {
+    ctx.body = 'getPosts';
+}

e1fc4e546c8bca8886d14f27de81e388b662f8d5
Lesta <295510545@qq.com>
2018-08-12 10:21:54 +0800

    过渡动画与动画

--- react/todolist/src/index.js
+++ react/todolist/src/index.js
@@ -2,12 +2,14 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TodoList from './TodoList';
+// import TodoList from './TodoList';
+import App from './App';
 
 //PWA progressive web applicattion  写网页形式写手机APP应用
 // https协议的服务器上 
 // import registerServiceWorker from './registerServiceWorker';
 
 // 将APP组件挂载到根结点下  <App />是jsx语法  头部需引入React才能解析
-ReactDOM.render(<TodoList />, document.getElementById('root'));
+// ReactDOM.render(<TodoList />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();

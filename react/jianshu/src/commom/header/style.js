06257661fbea4c1f36c4c30b8b1c4fed0de4aae9
Lesta <295510545@qq.com>
2018-08-23 14:01:50 +0800

    简单动画效果(react-transition-group)

--- react/jianshu/src/commom/header/style.js
+++ react/jianshu/src/commom/header/style.js
@@ -45,12 +45,43 @@ export const NavItem = styled.div`
     }
 `
 
+export const SearchWrapper = styled.div`
+    position: relative;
+    float: left;
+    .slide-enter {
+        width: 160px;
+        transition: all .2s ease-out;
+    }
+    .slide-enter-active {
+        width: 240px;
+    }
+    .slide-exit {
+        transition: all .2s ease-out;
+    }
+    .slide-exit-active {
+        width: 160px;
+    }
+    .iconfont {
+        position: absolute;
+        right: 5px;
+        bottom: 5px;
+        width: 30px;
+        line-height: 30px;
+        border-radius: 15px;
+        text-align: center;
+        &.focused {
+            background: #777;
+            color: #ffffff
+        }
+    }
+`
+
 export const NavSearch = styled.input.attrs({
     placeholder: '搜索'
 })`
     width: 160px;
     height: 38px;
-    padding: 0 20px
+    padding: 0 30px 0 20px;
     box-sizing: border-box;
     margin-top: 9px;
     margin-left: 20px
@@ -59,8 +90,12 @@ export const NavSearch = styled.input.attrs({
     border-radius: 19px;
     background: #eeeeee;
     font-size: 14px;
+    color: #777;
     &::placeholder {
         color: #999
+    }
+    &.focused {
+        width: 240px;
     }
 
 `

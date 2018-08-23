2b21fba75077d3fdc7f3556a0900a53717202edf
Lesta <295510545@qq.com>
2018-08-23 12:17:04 +0800

    react之简书,styled-components学习

---
+++ react/jianshu/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Header from './commom/header/index';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header/>
+      </div>
+    );
+  }
+}
+
+export default App;
